13b74dbd174861b130902ad908796f1a0de68890
Shinminhye <92992249+owo1214@users.noreply.github.com>
2021-10-23 20:27:53 +0900

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,24 @@
+package baekjoon.chap2;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		sc.close();
+		
+		if (A > B) {
+			System.out.println(">");
+		} else if (A < B){
+			System.out.println("<");
+		} else if (A == B) {
+			System.out.println("==");
+		}
+		
+	}
+
+}
---
+++ Q5.java
@@ -0,0 +1,17 @@
+package baekjoon.chap2;
+
+import java.util.Scanner;
+
+public class Q5 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int H = scanner.nextInt();
+		int M = scanner.nextInt();
+		
+		scanner.close();
+		
+	
+			
+	}	
+}
